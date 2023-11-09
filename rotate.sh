5c8e30e81b5a10680b78a56b837491b7dd803e24
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2023-11-09 12:27:52 -0800

    Changed wallpaper setting from feh to pcmanfm.

--- rotate.sh
+++ rotate.sh
@@ -6,6 +6,6 @@
 #!/bin/sh
 while true;
 do
-   find /media/pipi/e9535df1-d952-4d78-b5d7-b82e9aa3a975/Master -type f \( -name '*.jpg' -o -name '*.png' \) -print0 | shuf -n 1 -z | xargs -0 feh --bg-scale
+   find /media/pipi/e9535df1-d952-4d78-b5d7-b82e9aa3a975/Master -type f \( -name '*.jpg' -o -name '*.png' \) -print0 | shuf -n 1 -z | xargs -0 pcmanfm --wallpaper-mode=1
    sleep 2m
 done &
