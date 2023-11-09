66b375ef80b5c6c354ecfd70fff703efa3aa50dc
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2023-11-09 11:58:44 -0800

    Update wallpaper rotation script to use a random image from a specified directory and rotate every 2 minutes

--- rotate.sh
+++ rotate.sh
@@ -1,30 +1,23 @@
 #!/bin/bash
 
-# Set the directory containing the images
-DIR=/media/pipi/e9535df1-d952-4d78-b5d7-b82e9aa3a975/Master
+# Get the directory of photos to use for the wallpaper
+wallpaper_dir="/path/to/wallpaper/directory"
 
-# Get the number of images in the directory
-RANGE=$(ls -1 "$DIR"/*.jpg | wc -l)
+# Set the interval in minutes between wallpaper rotations
+interval=2
 
-# Set the minimum and maximum wait time in seconds
-MIN_WAIT=60
-MAX_WAIT=120
+# Get a list of all the images in the wallpaper directory
+images=$(ls "$wallpaper_dir"/*.jpg)
 
+# Choose a random image from the list
+random_image=$(shuf -n 1 <<< "$images")
+
+# Set the wallpaper to the chosen image
+lxc config set desktop wallpaper "$random_image"
+
+# Start a loop to rotate the wallpaper every 2 minutes
 while true; do
-    # Generate a random number between 1 and $RANGE
-    number=$(openssl rand -hex 4 | awk '{print strtonum("0x"substr($0,0,4))}')
-    let "number %= $RANGE"
-
-    # Set the wallpaper to the selected image
-    COUNTER=1
-    for X in "$DIR"/*.jpg; do
-        if [ $number -eq $COUNTER ]; then
-            # Use a different command to set the wallpaper if pcmanfm is not available
-            pcmanfm --set-wallpaper "$X"
-        fi
-        let "COUNTER++"
-    done
-
-    # Wait for a random amount of time between $MIN_WAIT and $MAX_WAIT seconds
-    sleep $(shuf -i $MIN_WAIT-$MAX_WAIT -n 1)
+  sleep "$interval"m
+  random_image=$(shuf -n 1 <<< "$images")
+  lxc config set desktop wallpaper "$random_image"
 done
