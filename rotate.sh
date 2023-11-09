8866958b69615f054716362245bba2e695d6f4fe
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2023-11-09 12:22:02 -0800

    Refactor wallpaper rotation script to use feh

--- rotate.sh
+++ rotate.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
 
 # Get the directory of photos to use for the wallpaper
-wallpaper_dir="/media/pipi/e9535df1-d952-4d78-b5d7-b82e9aa3a975/Master"
+# wallpaper_dir="/media/pipi/e9535df1-d952-4d78-b5d7-b82e9aa3a975/Master"
 
-# Set the interval in minutes between wallpaper rotations
-interval=2
-
-# Get a list of all the images in the wallpaper directory
-images=$(ls "$wallpaper_dir"/*.jpg)
-
-# Choose a random image from the list
-random_image=$(shuf -n 1 <<< "$images")
-
-# Set the wallpaper to the chosen image
-lxc config set desktop wallpaper "$random_image"
-
-# Start a loop to rotate the wallpaper every 2 minutes
-while true; do
-  sleep "$interval"m
-  random_image=$(shuf -n 1 <<< "$images")
-  lxc config set desktop wallpaper "$random_image"
-done
+#!/bin/sh
+while true;
+do
+   find /media/pipi/e9535df1-d952-4d78-b5d7-b82e9aa3a975/Master -type f \( -name '*.jpg' -o -name '*.png' \) -print0 | shuf -n 1 -z | xargs -0 feh --bg-scale
+   sleep 2m
+done &
